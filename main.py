6706b30eefb802690ac58533cec75e915084112f
singhjiya456789 <jiyasingh1603@gmail.com>
2026-05-29 11:09:43 +0545

    First commit

---
+++ main.py
@@ -0,0 +1,29 @@
+while True:
+    print("1.ADD")
+    print("2.Substract")
+    print("3.Multiply")
+    print("4.Divide")
+
+    choice=input("Enter the choice between 1-4:")
+    if choice =="1":
+        a=int(input("Enter the 1st no.:"))
+        b=int(input("Enter the 2nd no.:"))
+        print("Result:",a+b)
+    elif choice == "2":
+        a=int(input("Enter the 1st no.:"))
+        b=int(input("Enter the 2nd no.:"))
+        print("Result:",a-b)
+    elif choice == "3":
+        a=int(input("Enter the 1st no.:"))
+        b=int(input("Enter the 2nd no.:"))
+        print("Result:",a*b)
+    elif choice == "4":
+        a=int(input("Enter the dividend no. :"))
+        b=int(input("Enter the divider no. :"))
+        print("Result:",a/b)
+        break
+    else:
+        print("Invalid choice!")
+    print()
+
+
